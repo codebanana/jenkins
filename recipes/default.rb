d59b622b8b6681c5f2b7f0d2e0b3ba29b21d89f2
freefood89 <freefood89@gmail.com>
2016-03-30 10:43:41 +0000

    renamed cookbook to jenkins-server

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: jenkins
+# Cookbook Name:: jenkins-server
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,12 +1,12 @@
 #
-# Cookbook Name:: jenkins
+# Cookbook Name:: jenkins-server
 # Spec:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'jenkins::default' do
+describe 'jenkins-server::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
